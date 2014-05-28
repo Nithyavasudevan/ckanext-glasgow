04021db907109a5291833eb5ae96c45fb8d1802c
amercader <amercadero@gmail.com>
2014-05-28 17:41:56 +0100

    [#19] Add flask app mocking the EC API
    This is a small flask app that can be run while developing or as part of
    the tests to mock the EC API. It performs basic Authorization and
    Validation checks that mimic the spec provided by Microsoft so far.
    
    The responses are also in accordance to the MS spec.
    
    Current supported operations are Request dataset creation and Request
    dataset update.

---
+++ ckanext/glasgow/tests/mock_ec.py
@@ -0,0 +1,157 @@
+import uuid
+
+import flask
+from werkzeug.exceptions import default_exceptions
+from werkzeug.exceptions import HTTPException
+
+
+def make_json_app(import_name, **kwargs):
+    """
+    Creates a JSON-oriented Flask app.
+
+    All error responses that you don't specifically
+    manage yourself will have application/json content
+    type, and will contain JSON like this (just an example):
+
+    { "message": "405: Method Not Allowed" }
+    """
+    def make_json_error(ex):
+        response = flask.jsonify(Message=str(ex))
+        response.status_code = (ex.code
+                                if isinstance(ex, HTTPException)
+                                else 500)
+        return response
+
+    app = flask.Flask(import_name, **kwargs)
+
+    for code in default_exceptions.iterkeys():
+        app.error_handler_spec[None][code] = make_json_error
+
+    return app
+
+app = make_json_app(__name__)
+
+dataset_all_fields = [
+    'Category',
+    'Description',
+    'License',
+    'MaintainerContact',
+    'MaintainerName',
+    'OpennessRating',
+    'PublishedOnBehalfOf',
+    'Quality',
+    'StandardName',
+    'StandardRating',
+    'StandardVersion',
+    'Tags',
+    'Theme',
+    'Title',
+    'UsageGuidance',
+]
+
+dataset_mandatory_fields = [
+
+    'Title',
+    'Description',
+    'MaintainerName',
+    'MaintainerContact',
+    'License',
+    'OpennessRating',
+    'Quality',
+]
+
+dataset_fields_under_255_characters = [
+    'Title',
+    'MaintainerName',
+    'MaintainerContact',
+    'License',
+    'Category',
+    'PublishedOnBehalfOf',
+    'StandardName',
+    'StandardVersion',
+    'Theme',
+    'UsageGuidance',
+]
+
+
+@app.route('/datasets', methods=['POST'])
+def request_dataset_create():
+
+    return handle_dataset_request()
+
+
+@app.route('/datasets', methods=['PUT'])
+def request_dataset_update():
+
+    return handle_dataset_request()
+
+
+def handle_dataset_request():
+    data = flask.request.json
+
+    if not data:
+        response = flask.jsonify(
+            Message='No data received'
+        )
+        response.status_code = 400
+        return response
+
+    # Authorization
+
+    if (not 'Authorization' in flask.request.headers or
+       flask.request.headers['Authorization'] == 'unknown_token'):
+        response = flask.jsonify(
+            Message='Not Auhtorized'
+        )
+        response.status_code = 401
+        return response
+
+    # Basic Validation
+
+    for field in dataset_mandatory_fields:
+        if not data.get(field):
+            response = flask.jsonify(
+                Message='Missing fields',
+                ModelState={
+                    'model.' + field:
+                    ["The {0} field is required.".format(field)]
+                })
+            response.status_code = 400
+            return response
+
+    for field in dataset_fields_under_255_characters:
+        if len(data.get(field, '')) > 255:
+            response = flask.jsonify(
+                Message='Field too long',
+                ModelState={
+                    'model.' + field:
+                    ["{0} field must be shorter than 255 characters."
+                     .format(field)]
+                })
+            response.status_code = 400
+            return response
+
+    # All good, return a request id
+
+    return flask.jsonify(
+        RequestId=unicode(uuid.uuid4())
+    )
+
+
+@app.route('/')
+def api_description():
+
+    api_desc = {
+        'Request dataset creation': 'POST /datasets',
+        'Request dataset update': 'PUT /datasets',
+    }
+
+    return flask.jsonify(**api_desc)
+
+
+def run(**kwargs):
+    app.run(**kwargs)
+
+
+if __name__ == '__main__':
+    run(port=7070, debug=True)
