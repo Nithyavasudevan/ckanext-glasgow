983b11fd3afb8facaba9f0c900b8df784dee5ad4
amercader <amercadero@gmail.com>
2014-06-20 11:29:43 +0100

    Tag release 0.1.0

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
-version = '0.0'
+version = '0.1.0'
 
 setup(
     name='ckanext-glasgow',
     version=version,
-    description="CKAN extension for the Glasgow Open Data Portal",
+    description="CKAN extension for the Glasgow TSB Future Cities Portal",
     long_description='''
     ''',
     classifiers=[],
     keywords='',
-    author='',
-    author_email='',
-    url='http://data.glasgow.gov.uk',
+    author='Open Knowledge',
+    author_email='info@ckan.org',
+    url='https://github.com/okfn/ckanext-glasgow',
     license='AGPL',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['ckanext', 'ckanext.glasgow'],
@@ -25,6 +25,6 @@ setup(
         [ckan.plugins]
         # Add plugins here, e.g.
         glasgow_schema=ckanext.glasgow.plugins:GlasgowSchemaPlugin
-	ec_initial_harvester=ckanext.glasgow.harvesters.ec_harvester:EcInitialHarvester
+        ec_initial_harvester=ckanext.glasgow.harvesters.ec_harvester:EcInitialHarvester
     ''',
 )
