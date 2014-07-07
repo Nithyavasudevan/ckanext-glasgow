92357eccea2fcb6b39f44c10b552bd516a574031
joetsoi <joe.yeung.tsoi@gmail.com>
2014-07-07 18:36:01 +0100

    reverse list in versioning

--- ckanext/glasgow/helpers.py
+++ ckanext/glasgow/helpers.py
@@ -14,11 +14,17 @@ def get_resource_versions(dataset_id, resource_id):
             'session': model.Session,
             'user': toolkit.c.user,
         }
+        # we are provided revisions in ascending only by the EC API platform
+        # but the requirement is for a descending list, so we are reversing it
+        # here
         resource_versions_show = toolkit.get_action('resource_version_show')
-        return resource_versions_show(context, {
+        versions = resource_versions_show(context, {
             'package_id': dataset_id,
             'resource_id': resource_id,
         })
+        if versions:
+            versions.reverse()
+        return versions
     except (toolkit.ValidationError, toolkit.NotAuthorized, toolkit.ObjectNotFound), e:
 
         helpers.flash_error('{0}'.format(e.error_dict['message']))
