40f96cea8e8527e36e74e7d6ea5e3caeba1b4308
Sean Hammond <git@seanh.cc>
2014-07-26 15:10:26 +0000

    [#53] Some improvements to the create users form

--- ckanext/glasgow/controllers/create_users.py
+++ ckanext/glasgow/controllers/create_users.py
@@ -4,4 +4,13 @@ import ckan.plugins.toolkit as toolkit
 class CreateUsersController(toolkit.BaseController):
 
     def create_users(self):
-        return toolkit.render('create_users.html')
+
+        # Get the list of organisations that the logged-in user is an admin
+        # of.
+        # FIXME: This is just getting all organisations!
+        organisation_names = toolkit.get_action('organization_list')(
+            context=None, data_dict={})
+
+        return toolkit.render(
+            'create_users.html',
+            extra_vars={'organisation_names': organisation_names})
