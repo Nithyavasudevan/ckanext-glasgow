2f8606d443fe98bf97a5edec274ea28a0fb7099b
Sean Hammond <git@seanh.cc>
2014-07-26 18:57:23 +0000

    [#53] Actually post user create requests to the platform
    I haven't got this working yet, but not sure if it's my fault or the
    server's

--- ckanext/glasgow/controllers/create_users.py
+++ ckanext/glasgow/controllers/create_users.py
@@ -1,6 +1,11 @@
+import pylons
+import requests
+import json
+
 import ckan.model as model
 import ckan.new_authz as new_authz
 import ckan.plugins.toolkit as toolkit
+import ckan.lib.helpers as helpers
 
 
 def organizations_that_user_is_admin_of(user):
@@ -33,16 +38,61 @@ def organizations_that_user_is_admin_of(user):
 class CreateUsersController(toolkit.BaseController):
 
     def create_users(self):
+
+        # extra_vars has to contain 'data' or the template will crash.
+        # Replace data with a non-empty dict to pre-fill the form fields.
+        extra_vars = {'data': {}}
+
+        if toolkit.request.method == 'POST':
+
+            params = dict(toolkit.request.params)
+
+            organisation = params.pop('organisation')
+            organisation_id = toolkit.get_action('organization_show')(
+                context={}, data_dict={'id': organisation})['id']
+
+            # Get the URL to post to from the config file.
+            url = pylons.config.get('ckanext.glasgow.metadata_api')
+            if not url.endswith('/'):
+                url = url + '/'
+            url = url + 'Identity/Organisation/{organisation}/User'
+            url = url.format(organisation=organisation_id)
+
+            # TODO: Verify that passwords match.
+            del params['confirm-password']
+
+            # TODO: We could validate the params here, and re-render the form
+            # with errors if any fail to validate.
+
+            access_token = pylons.session.get(
+                'ckanext-oauth2waad-access-token')
+
+            try:
+                response = requests.post(
+                    url, data=json.dumps(params),
+                    headers={'Authorization': access_token})
+            except requests.ConnectionError as err:
+                helpers.flash_error(str(err))
+                extra_vars['data'] = dict(toolkit.request.params)
+
+            if response.status_code == 200:
+                helpers.flash_success('A request to create user {username} '
+                    'was successfully sent to The Platform.'.format(
+                    username=params['Username']))
+            else:
+                helpers.flash_error('The Platform returned an error: '
+                    '{status} {reason}'.format(
+                    status=str(response.status_code), reason=response.reason))
+                extra_vars['data'] = dict(toolkit.request.params)
+
         user = toolkit.c.user
-        is_sysadmin = new_authz.is_sysadmin(user)
+        extra_vars['is_sysadmin'] = new_authz.is_sysadmin(user)
 
-        if is_sysadmin:
-            organisation_names = toolkit.get_action('organization_list')(
-                context={}, data_dict={})
+        if extra_vars['is_sysadmin']:
+            extra_vars['organisation_names'] = toolkit.get_action(
+                'organization_list')(context={}, data_dict={})
         else:
-            organisation_names = organizations_that_user_is_admin_of(user)
+            extra_vars['organisation_names'] = (
+                organizations_that_user_is_admin_of(user))
 
-        return toolkit.render(
-            'create_users.html',
-            extra_vars={'organisation_names': organisation_names,
-                        'is_sysadmin': is_sysadmin})
+        return toolkit.render('create_users.html', extra_vars=extra_vars)
